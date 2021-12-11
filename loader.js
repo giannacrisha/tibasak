3c1b1e774364ab34da33a5e4e616d2fd0f1044ab
Gianna Crisha <giannacrisha@gmail.com>
2021-12-11 20:43:09 +0800

    naur

--- loader.js
+++ loader.js
@@ -2,3 +2,13 @@ let loaderDIV = document.querySelector(".loaderDIV");
 window.addEventListener('load', function(){
 	loaderDIV.style.display = 'none';
 });
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
